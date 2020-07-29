3512eb639605c16fa9940727f4b6a44911505b9c
softactor <tanveerqureshee1@gmail.com>
2020-07-29 20:51:29 +0600

    text commit

--- top_sidebar.php
+++ top_sidebar.php
@@ -19,7 +19,7 @@
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="vendor/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        <span class="hidden-xs">Admin</span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- Menu Footer-->
